97bdc04b2acf04b2c1832fd8104070cd30b2db77
Doshik <100679081+KiriokkonDon@users.noreply.github.com>
2022-07-05 14:21:50 +0300

    Fix

--- Math_Calculator/Math_Calculator.cpp
+++ Math_Calculator/Math_Calculator.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <stack>
+#include <sstream>
 #include <cmath>
 using namespace std;
 
@@ -24,12 +25,12 @@ double Ctg(double x) {
     double b = Sin(x);
     return (a / b);
 }
-bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
+bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_opp, Struct& item) {
 
     double a, b, c;
     a = Stack_ch.top().value;
     Stack_ch.pop();
-    switch (Stack_op.top().type)
+    switch (Stack_opp.top().type)
     {
     case '+':
         b = Stack_ch.top().value;
@@ -38,7 +39,7 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case '-':
         b = Stack_ch.top().value;
@@ -47,7 +48,7 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case '^':
         b = Stack_ch.top().value;
@@ -56,7 +57,7 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case '*':
         b = Stack_ch.top().value;
@@ -65,12 +66,12 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case '/':
         b = Stack_ch.top().value;
         if (a == 0) {
-            cout << " \nError\n";
+            cerr << "\nНа 0 делить нельзя!\n";
             return false;
         }
         Stack_ch.pop();
@@ -78,25 +79,25 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case 's':
         c = Sin(a);
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case 'c':
         c = Cos(a);
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     case 't':
         if (Cos(a) == 0) {
-            cout << "\nError\n";
+            cerr << "\nНеверный аргумент для тангенса!\n";
             return false;
         }
         else {
@@ -104,13 +105,13 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
             item.type = '0';
             item.value = c;
             Stack_ch.push(item);
-            Stack_op.pop();
+            Stack_opp.pop();
             break;
         }
 
     case 'g':
         if (Sin(a) == 0) {
-            cout << "\nError\n";
+            cerr << "\nНеверный аргумент для котангенса!\n";
             return false;
         }
         else {
@@ -118,7 +119,7 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
             item.type = '0';
             item.value = c;
             Stack_ch.push(item);
-            Stack_op.pop();
+            Stack_opp.pop();
             break;
         }
     case 'e':
@@ -126,10 +127,10 @@ bool Math(stack <Struct>& Stack_ch, stack <Struct>& Stack_op, Struct& item) {
         item.type = '0';
         item.value = c;
         Stack_ch.push(item);
-        Stack_op.pop();
+        Stack_opp.pop();
         break;
     default:
-        cout << "Error";
+        cerr << "\nОшибка!\n";
         return false;
         break;
     }
@@ -145,135 +146,140 @@ int Rang(char ch) {//Приоритет
 }
 int main()
 {
-    char ch;
-    double value;
-    bool flag = 1;
-    stack <Struct> Stack_ch; //Стэк с числами
-    stack <Struct> Stack_op; //Стэк с операциями
-    Struct item;
-    while (1)
-    {
-        ch = cin.peek();
-        if (ch == '\n')break;
-        if (ch == ' ') {
-            cin.ignore();
-            continue;
-        }
-        if (ch == 's' || ch == 'c' || ch == 't' || ch == 'e') {
-            char foo[3];
-            for (int i = 0; i < 3; i++)
-            {
-                ch = cin.peek();
-                foo[i] = ch;
-                cin.ignore();
-            }
-            if (foo[0] == 's' && foo[1] == 'i' && foo[2] == 'n') {
-                item.type = 's';
-                item.value = 0;
-                Stack_op.push(item);
-                continue;
-            }
-            if (foo[0] == 'c' && foo[1] == 'o' && foo[2] == 's') {
-                item.type = 'c';
-                item.value = 0;
-                Stack_op.push(item);
-                continue;
-            }
-            if (foo[0] == 't' && foo[1] == 'a' && foo[2] == 'n') {
-                item.type = 't';
-                item.value = 0;
-                Stack_op.push(item);
-                continue;
-            }
-            if (foo[0] == 'c' && foo[1] == 't' && foo[2] == 'g') {
-                item.type = 'g';
-                item.value = 0;
-                Stack_op.push(item);
-                continue;
-            }
-            if (foo[0] == 'e' && foo[1] == 'x' && foo[2] == 'p') {
-                item.type = 'e';
-                item.value = 0;
-                Stack_op.push(item);
-                continue;
-            }
-        }
+    setlocale(LC_ALL, "rus");
+    while (true) {
+        system("cls");
+        cout << "   Введите выражение: ";
+        std::string str;
+        getline(cin, str);
+        stringstream sstr{ str };
 
-        if (ch == 'p') {
-            cin >> value;
-            item.type = '0';
-            item.value = Pi;
-            Stack_ch.push(item);
-            flag = 0;
-            cin.ignore();
-            continue;
-        }
-
-        if (ch >= '0' && ch <= '9' || ch == '-' && flag == 1) {
-            cin >> value;
-            item.type = '0';
-            item.value = value;
-            Stack_ch.push(item);
-            flag = 0;
-            continue;
-        }
-        if (ch == '+' || ch == '-' && flag == 0 || ch == '*' || ch == '/' || ch == '^') {
-            if (Stack_op.size() == 0) {
+        char ch;
+        double value;
+        bool flag = true;
+        stack<Struct> Stack_ch; 
+        stack<Struct> Stack_op; 
+        Struct item;
+        while (true) {
+            ch = sstr.peek();
+            if (ch == '\377')break;
+            if (ch == ' ') {
+                sstr.ignore();
+                continue;
+            }
+            if (ch == 's' || ch == 'c' || ch == 't' || ch == 'e') { 
+                char foo[3];
+                for (int i = 0; i < 3; i++) {
+                    ch = sstr.peek();
+                    foo[i] = ch;
+                    sstr.ignore();
+                }
+                if (foo[0] == 's' && foo[1] == 'i' && foo[2] == 'n') { 
+                    item.type = 's';
+                    item.value = 0;
+                    Stack_op.push(item); 
+                    continue;
+                }
+                if (foo[0] == 'c' && foo[1] == 'o' && foo[2] == 's') {
+                    item.type = 'c';
+                    item.value = 0;
+                    Stack_op.push(item);
+                    continue;
+                }
+                if (foo[0] == 't' && foo[1] == 'a' && foo[2] == 'n') {
+                    item.type = 't';
+                    item.value = 0;
+                    Stack_op.push(item);
+                    continue;
+                }
+                if (foo[0] == 'c' && foo[1] == 't' && foo[2] == 'g') {
+                    item.type = 'g';
+                    item.value = 0;
+                    Stack_op.push(item);
+                    continue;
+                }
+                if (foo[0] == 'e' && foo[1] == 'x' && foo[2] == 'p') {
+                    item.type = 'e';
+                    item.value = 0;
+                    Stack_op.push(item);
+                    continue;
+                }
+            }
+            if (ch == 'p') {
+                item.type = '0';
+                item.value = Pi;
+                Stack_ch.push(item);
+                flag = 0;
+                sstr.ignore();
+                continue;
+            }
+            if (ch >= '0' && ch <= '9' || ch == '-' && flag == 1) {
+                sstr >> value;
+                item.type = '0';
+                item.value = value;
+                Stack_ch.push(item); 
+                flag = 0;
+                continue;
+            }
+            if (ch == '+' || ch == '-' && flag == 0 || ch == '*' || ch == '/' || ch == '^') {
+                if (Stack_op.size() == 0) {
+                    item.type = ch;
+                    item.value = 0;
+                    Stack_op.push(item);
+                    sstr.ignore();
+                    continue;
+                }
+                if (Stack_op.size() != 0 && Rang(ch) >
+                    Rang(Stack_op.top().type)) { 
+                    item.type = ch;
+                    item.value = 0;
+                    Stack_op.push(item); 
+                    sstr.ignore();
+                    continue;
+                }
+                if (Stack_op.size() != 0 && Rang(ch) <= Rang(Stack_op.top().type)) {
+                    if (Math(Stack_ch, Stack_op, item) == false) { 
+                        system("pause");
+                        return 0;
+                    }
+                    continue;
+                }
+            }
+            if (ch == '(') {
                 item.type = ch;
                 item.value = 0;
-                Stack_op.push(item);
-                cin.ignore();
+                Stack_op.push(item); 
+                sstr.ignore();
                 continue;
             }
-            if (Stack_op.size() != 0 && Rang(ch) > Rang(Stack_op.top().type)) {
-                item.type = ch;
-                item.value = 0;
-                Stack_op.push(item);
-                cin.ignore();
-                continue;
-            }
-            if (Stack_op.size() != 0 && Rang(ch) <= Rang(Stack_op.top().type)) {
-                if (Math(Stack_ch, Stack_op, item) == false) {
-                    system("pause");
-                    return 0;
+            if (ch == ')') { 
+                while (Stack_op.top().type != '(') {
+                    if (Math(Stack_ch, Stack_op, item) == false) { 
+                        system("pause");
+                        return 0;
+                    }
+                    else continue; 
                 }
+                Stack_op.pop();
+                sstr.ignore();
                 continue;
             }
-        }
-        if (ch == '(') {
-            item.type = ch;
-            item.value = 0;
-            Stack_op.push(item);
-            cin.ignore();
-            continue;
-        }
-        if (ch == ')') {
-            while (Stack_op.top().type != '(')
-            {
-                if (Math(Stack_ch, Stack_op, item) == false) {
-                    system("pause");
-                    return 0;
-                }
-                else continue;
+            else {
+                cout << "\nНеверно введено выражение!\n";
+                system("pause");
+                return 0;
             }
-            Stack_op.pop();
-            cin.ignore();
-            continue;
         }
-        else {
-            cout << "\nError\n";
-            system("pause");
-            return 0;
+        while (Stack_op.size() !=
+            0) {
+            if (Math(Stack_ch, Stack_op, item) == false) {
+                system("pause");
+                return 0;
+            }
+            else continue; 
         }
+        cout << "   Ответ: " << Stack_ch.top().value << endl;
+        system("pause");
     }
-    while (Stack_op.size() != 0) {
-        if (Math(Stack_ch, Stack_op, item) == false) {
-            system("pause");
-            return 0;
-        }
-        else continue;
-    }
-    cout << Stack_ch.top().value << endl;
     return 0;
-
 }
